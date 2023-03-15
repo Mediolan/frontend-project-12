126a6a05d77f2d7786504f5db3987d7afb331cc7
Denis Milano <milano@MilanoPC.localdomain>
2023-03-15 22:14:11 +0300

    change home and chat page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useEffect } from 'react';
+import { Button, Spinner } from 'react-bootstrap';
 import {
   Link, Outlet,
 } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 import { useAuth } from '../context/index.jsx';
 
 const Home = () => {
   const { t } = useTranslation();
   const { logOut, user } = useAuth();
+  const { loadingStatus, error } = useSelector((state) => state.loader);
 
-  return (
-    <>
-      <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-        <div className="container">
-          <Link className="navbar-brand" to="/">{t('header.hexletChat')}</Link>
-          {!!user && <Button type="button" onClick={logOut}>{t('header.logout')}</Button>}
-        </div>
-      </nav>
-      <Outlet />
-    </>
+  useEffect(() => {
+    if (loadingStatus === 'failed') {
+      logOut();
+      if (!error.isAxiosError) {
+        toast.error(t('toast.unknownErr'));
+      }
+      toast.error(t('toast.networkErr'));
+    }
+  }, [loadingStatus, logOut, error, t]);
+
+  return (loadingStatus === 'loading'
+    ? (
+      <div className="h-100 d-flex justify-content-center align-items-center">
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">{t('loading')}</span>
+        </Spinner>
+      </div>
+    )
+    : (
+      <>
+        <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+          <div className="container">
+            <Link className="navbar-brand" to="/">{t('header.hexletChat')}</Link>
+            {!!user && <Button type="button" onClick={logOut}>{t('header.logout')}</Button>}
+          </div>
+        </nav>
+        <Outlet />
+      </>
+    )
   );
 };
 
--- frontend/src/pages/chat/Chat.jsx
+++ frontend/src/pages/chat/Chat.jsx
@@ -1,52 +1,22 @@
-import React, { useEffect } from 'react';
-import Spinner from 'react-bootstrap/Spinner';
-import { useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
-import { toast } from 'react-toastify';
+import React from 'react';
 import ChatBox from './components/ChatBox';
 import ChannelsBox from './components/ChannelsBox';
 import Modals from './components/Modal';
-import { useAuth } from '../../context';
 
-const Chat = () => {
-  const { t } = useTranslation();
-  const { loadingStatus, error } = useSelector((state) => state.loader);
-  const { logOut } = useAuth();
-
-  useEffect(() => {
-    if (loadingStatus === 'failed') {
-      logOut();
-      if (!error.isAxiosError) {
-        toast.error(t('toast.unknownErr'));
-      }
-      toast.error(t('toast.networkErr'));
-    }
-  }, [loadingStatus, logOut, error, t]);
-
-  return (loadingStatus === 'loading'
-    ? (
-      <div className="h-100 d-flex justify-content-center align-items-center">
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">{t('loading')}</span>
-        </Spinner>
-      </div>
-    )
-    : (
-      <>
-        <Modals />
-        <div className="container h-100 my-4 overflow-hidden rounded shadow">
-          <div className="row h-100 bg-white flex-md-row">
-            <div className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
-              <ChannelsBox />
-            </div>
-            <div className="col p-0 h-100">
-              <ChatBox />
-            </div>
-          </div>
+const Chat = () => (
+  <>
+    <Modals />
+    <div className="container h-100 my-4 overflow-hidden rounded shadow">
+      <div className="row h-100 bg-white flex-md-row">
+        <div className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
+          <ChannelsBox />
         </div>
-      </>
-    )
-  );
-};
+        <div className="col p-0 h-100">
+          <ChatBox />
+        </div>
+      </div>
+    </div>
+  </>
+);
 
 export default Chat;
